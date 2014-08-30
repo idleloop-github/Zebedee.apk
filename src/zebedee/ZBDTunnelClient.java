e3d9c83adc49d51ce6a0e720f15c4d35cfac426b
idleloop-github <idleloop-github@users.noreply.github.com>
2014-08-30 23:59:23 +0200

    Update ZBDTunnelClient.java
    oops, vi edition error corrected.

--- src/zebedee/ZBDTunnelClient.java
+++ src/zebedee/ZBDTunnelClient.java
@@ -1,4 +1,4 @@
-ii// Copyright (C) 2001, Internet Designers Ltd. All rights reserved.
+// Copyright (C) 2001, Internet Designers Ltd. All rights reserved.
 // This program is free software and may be distributed under the terms
 // of the GNU Lesser General Public License, Version 2.1.
 // This program comes with ABSOLUTELY NO WARRANTY.
